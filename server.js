f73f3add74b2b39b7c13819710de8a60c98cadfb
hunter-xc <kwok.chak.shun@hotmail.com>
2018-01-18 13:33:16 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -90,12 +90,8 @@ app.post("/login", function(req, res) {
 });
 
 app.get('/logout', function(req, res) {
-	req.session.destroy(function(err) {
-		if (err)
-			console.log(err);
-		else 
-			res.redirect('/');
-	}
+	req.session = null;
+	res.redirect('/');
 });
 
 app.get('/register', function(req, res) {
