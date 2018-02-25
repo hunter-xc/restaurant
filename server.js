661664de84b033355ca8a0a76395001611a553bb
hunter-xc <kwok.chak.shun@hotmail.com>
2018-02-26 00:31:54 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -379,6 +379,8 @@ app.get('/read_schedule', function(req, res) {
 
 
 app.get('/read_schedule2', function(req, res) {
+	res.render('read_schedule2.ejs');
+	/*
 	MongoClient.connect(mongourl, function(err, db) {
 		assert.equal(err, null);
 		read_schedule(db, {'userid': req.session.username}, function(result) {
@@ -388,6 +390,7 @@ app.get('/read_schedule2', function(req, res) {
 			});
 		});		
 	});
+	*/
 });
 
 /*
